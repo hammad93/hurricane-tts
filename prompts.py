571546e4a5b05416d1cb95cbba808e2a7bc4f181
Hammad Usmani <ham.u@live.com>
2023-11-05 21:21:12 +0000

    Add prompts that were working in the OpenAI playground.

--- prompts.py
+++ prompts.py
@@ -1,5 +1,10 @@
 import utils
 
-def lang_list():
+def lang_list(col = 'Language Name'):
     langs = utils.tts_langs()
-    return set(x['Language Name'])
+    return set(x[col])
+
+def generate_prompts():
+    # extract and transform data structures
+    langs = lang_list()
+    storms = utils.transform_storm_data()
