1d3ac8551acb4688f50a86e143729e61505ff793
Hammad Usmani <ham.u@live.com>
2023-11-05 21:20:29 +0000

    Remove unused column

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 supported_langs_html = 'https://dl.fbaipublicfiles.com/mms/misc/language_coverage_mms.html'
-supported_langs_out_cols = ['ISO 693-3', 'Language Name', 'Subgrouping']
+supported_langs_out_cols = ['ISO 693-3', 'Language Name']
 api_url_track = 'http://fluids.ai:1337/live-storms/'
 api_url_forecast = 'http://fluids.ai:1337/forecasts/'
 max_storm_history = 10 # it can be inefficient to include all storm history records and this is a limit
