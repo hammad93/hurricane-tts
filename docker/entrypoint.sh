8d819353b0098887dd138c6c0cd7d907e4cf3160
Hammad Usmani <ham.u@live.com>
2023-11-13 01:47:53 +0000

    Completed working containerization but #3 should be kept open for final comment

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,4 +6,8 @@ echo "Docker container has been started"
 declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
 
 wget https://raw.githubusercontent.com/hammad93/hurricane-tts/main/hurricane_tts.ipynb
+
+pip install numpy==1.23.5
+pip install ipython
+pip install nbformat
 ipython --TerminalIPythonApp.file_to_run=hurricane_tts.ipynb
